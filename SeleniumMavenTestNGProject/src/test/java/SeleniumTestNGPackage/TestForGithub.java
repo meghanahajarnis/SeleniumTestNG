dc326aebc9b855a152ea9977d10956b77e122035
AMOL HAJARNIS <AMOL HAJARNIS@LAPTOP-7D8OLH25>
2023-08-17 14:38:37 +0530

    class updated with message

--- SeleniumMavenTestNGProject/src/test/java/SeleniumTestNGPackage/TestForGithub.java
+++ SeleniumMavenTestNGProject/src/test/java/SeleniumTestNGPackage/TestForGithub.java
@@ -3,7 +3,7 @@ package SeleniumTestNGPackage;
 public class TestForGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
 
 	}
 
